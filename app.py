83ca8283180f55e7491942724e036964b545eff6
Dhananjai Singh <dhananjai.eee@gmail.com>
2024-05-27 18:16:27 +0530

    code update

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ from langchain.chat_models import ChatOpenAI
 
 from langchain.docstore.document import Document
 from langchain.memory import ChatMessageHistory, ConversationBufferMemory
-
+from dotenv import load_dotenv
 import chainlit as cl
+
+load_dotenv()
+
+openai_api_key = os.getenv('openai_api_key')
+os.environ['openai_api_key'] = openai_api_key
